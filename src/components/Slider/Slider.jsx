0f8ece38daa1636bdb276fd077196935ce61ca07
mutonyilewis <mutonyilewis@gmail.com>
2023-11-21 23:23:25 +0300

    Started on Slider for homepage

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Slider.scss'
+
+const Slider = () => {
+
+
+    const data = [
+        "https",
+        "http",
+        "ftp",
+        "ssh",
+    ]
+  return (
+    <div className='slider'>Slider</div>
+  )
+}
+
+export default Slider
